7976242da4b1cf1548154af47d94593e692b7a49
Liu Min <liuminish@gmail.com>
2020-10-01 20:45:19 +0800

    solved the first challenged! Acquired title 'First Challenger of the Kingdoms!'

--- move-all-zeros-to-end/solution.js
+++ move-all-zeros-to-end/solution.js
@@ -1,5 +1,20 @@
-const moveZeroes = () => {
+const moveZeroes = (arr) => {
   // Write solution here
+  let indexes = []
+  let allZeros = []
+  arr.reduce((acc, curr, currIndex) => {
+    if (curr === 0) {
+      indexes.push(currIndex)
+      allZeros.push(0)
+    }
+  }, 0)
+
+  indexes.reverse().reduce((acc, curr) => {
+    arr.splice(curr, 1)
+  }, 0)
+
+  return arr.concat(allZeros)
 }
+
 
 export default moveZeroes
