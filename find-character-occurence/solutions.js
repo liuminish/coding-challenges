f60c828c022586b91da2a6c340afc21aa819bdf6
Liu Min <liuminish@gmail.com>
2020-10-01 23:51:35 +0800

    sucessfully solved more challenges! Acquired title -Endless Perserverance of the Genius

--- find-character-occurence/solutions.js
+++ find-character-occurence/solutions.js
@@ -1,5 +1,14 @@
-const findCharacterOccurence = () => {
-  
+const findCharacterOccurence = (string, char) => {
+  const arr = string.split('')
+  let count = 0
+  arr.reduce((acc, curr) => {
+    if (curr === char) {
+        count = count + 1
+    }
+  }, 0)
+
+  return count;
 }
+
 
 export default findCharacterOccurence
--- pig-latin/solution.js
+++ pig-latin/solution.js
@@ -1,5 +1,32 @@
-const pigLatin = () => {
-  
+const pigLatin = (word) => {
+    let vowels = ['a','e','i','o','u','A','E','I','O','U']
+    let end = 'ay'
+    let mid = []
+    let indexes = []
+
+    const arr = word.split('')
+
+    for (let i = 0; i < arr.length; i++) {
+        let char = arr[i]
+        let found = vowels.find(vowel => vowel === char)
+
+        if (found && i === 0) {
+            mid.push('y')
+            break;
+        } else if (!found) {
+            indexes.push(i)
+            mid.push(char)
+        } else {
+            break;
+        }
+        
+    }
+
+    indexes.reverse().forEach(index => {
+        arr.splice(index, 1)
+    })
+
+    return arr.join('').concat(mid.join(''),end)
 }
 
 export default pigLatin
